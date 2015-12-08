67d489b69ad445fad570831a9a9111e6cb78a9fa
ElliottOlson <elliotto17258@apps.edina.k12.mn.us>
2015-12-07 22:42:43 -0600

    Fixed build scripts.

--- src/com/edinarobotics/westcoast/Components.java
+++ src/com/edinarobotics/westcoast/Components.java
@@ -8,17 +8,16 @@ public class Components {
 
     public Drivetrain drivetrain;
 
-    private final int TOP_LEFT_CANTALON = 1;
-    private final int TOP_RIGHT_CANTALON = 2;
-    private final int BOTTOM_LEFT_CANTALON = 2;
-    private final int BOTTOM_RIGHT_CANTALON = 4;
+    private final int TOP_LEFT_CANTALON = 2;
+    private final int TOP_RIGHT_CANTALON = 3;
+    private final int BOTTOM_LEFT_CANTALON = 4;
+    private final int BOTTOM_RIGHT_CANTALON = 5;
 
     private Components() {
-        BOTTOM_LEFT_CANTALON = 3;
-        drivetrain = new _______(TOP_LEFT_CANTALON, BOTTOM_LEFT_CANTALON, TOP_RIGHT_CANTALON, BOTTOM_RIGHT_CANTALON);
+        drivetrain = new Drivetrain(TOP_LEFT_CANTALON, BOTTOM_LEFT_CANTALON, TOP_RIGHT_CANTALON, BOTTOM_RIGHT_CANTALON);
     }
 
-    public static ________ getInstance() {
+    public static Components getInstance() {
         if (instance == null) {
             instance = new Components();
         }
