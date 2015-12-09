9136927cc99211db507677f402cf685f6ff06ac3
ElliottOlson <elliotto17258@apps.edina.k12.mn.us>
2015-12-08 22:46:35 -0600

    Working West-Coast drive with 6 CANTalons.

--- src/com/edinarobotics/westcoast/subsystems/Drivetrain.java
+++ src/com/edinarobotics/westcoast/subsystems/Drivetrain.java
@@ -1,29 +1,38 @@
 package com.edinarobotics.westcoast.subsystems;
 
+import com.edinarobotics.utils.controllers.SpeedControllerWrapper;
 import com.edinarobotics.utils.subsystems.Subsystem1816;
+import com.edinarobotics.utils.wheel.Wheel;
+
 import edu.wpi.first.wpilibj.CANTalon;
-import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class Drivetrain extends Subsystem1816 {
 
-    private CANTalon topLeft, topRight, bottomLeft, bottomRight;
+	private Wheel topLeft, middleLeft, bottomLeft, topRight, middleRight, bottomRight;
+    private SpeedControllerWrapper left, right;
+    
     private double leftStick, rightStick;
 
-    private RobotDrive robotDrive;
+    public Drivetrain(int topLeft, int middleLeft, int bottomLeft, int topRight, int middleRight, int bottomRight) {
+       
+    	this.topLeft = new Wheel("Top Left", new CANTalon(topLeft), false);
+    	this.middleLeft = new Wheel("Middle Left", new CANTalon(middleLeft), false);
+    	this.bottomLeft = new Wheel("Bottom Left", new CANTalon(bottomLeft), false);
+    	this.topRight = new Wheel("Top Right", new CANTalon(topRight), false);
+    	this.middleRight = new Wheel("Middle Right", new CANTalon(middleRight), true);
+    	this.bottomRight = new Wheel("Bottom Right", new CANTalon(bottomRight), true);
 
-    public Drivetrain(int topLeft, int bottomLeft, int topRight, int bottomRight) {
-        this.topLeft = new CANTalon(topLeft);
-        this.topRight = new CANTalon(topRight);
-        this.bottomLeft = new CANTalon(bottomLeft);
-        this.bottomRight = new CANTalon(bottomRight);
-
-        robotDrive = new RobotDrive(this.bottomLeft, this.bottomRight);
+    	//To invert a wheel: this.bottomRight.setReversed(true); or change false to true above.
+    	
+    	left = new SpeedControllerWrapper(this.topLeft, this.middleLeft, this.bottomLeft);
+    	right = new SpeedControllerWrapper(this.topRight, this.middleRight, this.bottomRight);
     }
 
     @Override
     public void update() {
-        robotDrive.tankDrive(leftStick, rightStick);
+    	left.set(-leftStick);
+    	right.set(-rightStick);
     }
 
     public void setDefaultCommand(Command command){
